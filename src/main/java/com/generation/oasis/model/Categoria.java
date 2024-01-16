232fe3a39f8d28f0d504eef6fd39b6a65fece9ea
Anderson <ander_son12@hotmail.com>
2024-01-16 18:24:12 -0300

    Implementando application.properties e classe categoria]

---
+++ src/main/java/com/generation/oasis/model/Categoria.java
@@ -0,0 +1,36 @@
+package com.generation.oasis.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotBlank (message = "O atributo tipo é Obrigatório")
+	private String tipo;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+}
